43316490b6b67f4794b1759a62ba312c4646a10d
Thomas Orozco <thomasorozcog@gmail.com>
2023-09-26 08:22:20 -0700

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace EjercicioRecursivosSLE26Sep
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Recu recu1 = new Recu();
+
+            Console.WriteLine("Introducce el numero para el valor DESCEDENTE");
+            int numeroUsuario= Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(recu1.NumeroDescendente(numeroUsuario));
+
+
+
+            Console.WriteLine("Escribe el numero para valor ascedente");
+            int numeroUsuario1= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(recu1.NumeroAscedente(numeroUsuario1));
+        }
+    }
+}
---
+++ Recu.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioRecursivosSLE26Sep
+{
+    internal class Recu
+    {
+        //PARA UN EJERCICIO RECURSIVO SE NECESITA QUE SE LLAME AL MISMO M
+        public int NumeroDescendente(int numeroUsuario)
+        {
+           
+                
+            if (numeroUsuario == 0)
+            {
+                return numeroUsuario;
+            }
+
+            Console.WriteLine(numeroUsuario);
+
+            return  NumeroDescendente(numeroUsuario-1);
+
+        }
+
+        public int NumeroAscedente(int numeroUsuario1)
+        {
+            if (numeroUsuario1==0)
+            {
+                return numeroUsuario1;
+            }
+            Console.WriteLine(numeroUsuario1);
+            return NumeroAscedente(numeroUsuario1+1);
+        }
+
+    }
+
+
+
+
+
+    
+}
